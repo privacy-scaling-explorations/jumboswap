6d1117bb3cf064d3232b7b75e43f62fe3ddfc2a8
Andrew Morris <voltrevo@gmail.com>
2025-03-24 13:40:41 +1100

    Remove unused things in Ctx

--- src/Ctx.ts
+++ src/Ctx.ts
@@ -1,12 +1,7 @@
 import z from 'zod';
 import { createContext, useContext } from 'react';
 import UsableField from './UsableField';
-import Emitter from './Emitter';
-import AsyncQueue from './AsyncQueue';
-import runProtocol from './runProtocol';
-import { makeZodChannel } from './ZodChannel';
-import { MessageReady } from './MessageTypes';
-import { Key, RtcPairSocket } from 'rtc-pair-socket';
+import { Key } from 'rtc-pair-socket';
 import Room, { IRoom } from './Room';
 import EcdhKeyPair from './EcdhKeyPair';
 import PartyTracker from './PartyTracker';
@@ -24,8 +19,6 @@ type PageKind =
   | 'Calculating'
   | 'Result'
   | 'Error';
-
-export type GameOption = 'rock' | 'paper' | 'scissors' | 'lizard' | 'spock';
 
 export const rtcConfig = (() => {
   const envVar = import.meta.env.VITE_RTC_CONFIGURATION;
@@ -54,25 +47,17 @@ const FinalizeNamesAndItems = z.object({
   ),
 });
 
-export default class Ctx extends Emitter<{ ready(choice: GameOption): void }> {
+export default class Ctx {
   page = new UsableField<PageKind>('Home');
   mode = new UsableField<'Host' | 'Join'>('Host');
   roomCode = new UsableField(Key.random().base58());
-  socket = new UsableField<RtcPairSocket | undefined>(undefined);
-  friendReady = false;
-  result = new UsableField<'win' | 'lose' | 'draw' | undefined>(undefined);
   errorMsg = new UsableField<string>('');
-  choice = new UsableField<GameOption | undefined>(undefined);
   mpcProgress = new UsableField<number>(0);
   partyTracker?: PartyTracker;
   name = new UsableField('');
   item = new UsableField('');
   parties = new UsableField<Party[]>([{ name: '', item: '', ready: false }]);
   room?: IRoom;
-
-  constructor() {
-    super();
-  }
 
   async host() {
     this.mode.set('Host');
@@ -144,76 +129,11 @@ export default class Ctx extends Emitter<{ ready(choice: GameOption): void }> {
     this.page.set('ChooseItems');
   }
 
-  async runProtocol(socket: RtcPairSocket) {
-    this.page.set('Lobby');
-
-    const msgQueue = new AsyncQueue<unknown>();
-
-    const FriendMsg = z.object({
-      from: z.literal(this.mode.value === 'Host' ? 'joiner' : 'host'),
-    });
-
-    const msgListener = (msg: unknown) => {
-      if (!FriendMsg.safeParse(msg).error) {
-        msgQueue.push(msg);
-      }
-    };
-
-    socket.on('message', msgListener);
-
-    const channel = makeZodChannel(
-      (msg: unknown) => socket.send(msg),
-      () => msgQueue.shift(),
-    );
-
-    const [choice, _readyMsg] = await Promise.all([
-      new Promise<GameOption>(resolve => {
-        this.once('ready', resolve);
-      }),
-      channel.recv(MessageReady).then(msg => {
-        this.friendReady = true;
-        return msg;
-      }),
-    ]);
-
-    this.page.set('Calculating');
-    socket.off('message', msgListener);
-
-    const result = await runProtocol(
-      this.mode.value,
-      socket,
-      choice,
-      percentage => {
-        this.mpcProgress.set(percentage);
-      },
-    );
-
-    this.result.set(result);
-
-    socket.close();
-
-    this.page.set('Result');
-  }
-
   handleProtocolError = (error: unknown) => {
     console.error('Protocol error:', error);
     this.errorMsg.set(`Protocol error: ${JSON.stringify(error)}`);
     this.page.set('Error');
   };
-
-  async send(choice: GameOption) {
-    this.emit('ready', choice);
-    this.choice.set(choice);
-
-    if (!this.friendReady) {
-      this.page.set('Waiting');
-    }
-
-    this.socket.value!.send({
-      from: this.mode.value === 'Host' ? 'host' : 'joiner',
-      type: 'ready',
-    });
-  }
 
   private static context = createContext<Ctx>(
     {} as Ctx,
--- src/Emitter.ts
+++
@@ -1,8 +0,0 @@
-import { EventEmitter } from 'events';
-import TypedEmitter, { EventMap } from 'typed-emitter';
-
-const Emitter = EventEmitter as {
-  new <T extends EventMap>(): TypedEmitter<T>;
-};
-
-export default Emitter;
